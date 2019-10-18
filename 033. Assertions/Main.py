ba749debd04b05e54497d7309fe93afa1113f870
FrogGreen <35386116+FrogGreen@users.noreply.github.com>
2019-10-18 23:04:27 +0200

    Add files via upload

---
+++ 033. Assertions/Main.py
@@ -0,0 +1,9 @@
+# Assertions
+
+weight = 15;
+
+assert weight <= 20 , "Ooops, here is overweight"
+print("Value of weight is " + str(weight) + ", it's look, that everything is fine.")
+
+assert weight >= 20 , "Ooops, here is underweight"
+print("Value of weight is " + str(weight))
