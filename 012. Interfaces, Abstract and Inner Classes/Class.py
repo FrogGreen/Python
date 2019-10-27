e2a682ff56d7cca3d37189fbf72dfb05b1b0e524
FrogGreen <35386116+FrogGreen@users.noreply.github.com>
2019-10-28 00:00:04 +0100

    Add files via upload

--- 012. Interfaces, Abstract and Inner Classes/Class.py
+++ 012. Interfaces, Abstract and Inner Classes/Class.py
@@ -1,17 +1,108 @@
-class Engine(object):
+from abc import ABC, abstractmethod
 
-    def __init__(self, percentOfMaxSpeed=0, horsePower=0, cylinders=0, valve=0, isOn=False):
-        self.__percentOfMaxSpeed = percentOfMaxSpeed
-        self.__horsePower = horsePower
-        self.__cylinders = cylinders
-        self.__valve = valve
-        self.__isOn = isOn
+class InterfaceOne(object):
 
-    def isOn(self):
-        return "Engine is on" if self.__isOn else "Engine stop"
+    interfaceOne = "InterfaceOne - > Interface allow to have constant variables"
+
+    def InterfaceOne(self):
+        return self.interfaceOne
+
+    @classmethod
+    def print1(self):
+        print("InterfaceOne - > This is the different between DEFAULT and abstract method")
+
+    @classmethod
+    def print2(self):
+        print("InterfaceOne - > This is the different between DEFAULT and default")
+
+    @classmethod
+    def print3(self):
+        print("InterfaceOne - > This is the different between DEFAULT and static")
+
+    @staticmethod
+    def print4(self):
+        print("InterfaceOne - > This is the different between STATIC and static")
+
+    @classmethod
+    def print6(self):
+        print(self.InterfaceOne(self))
+        self.print1(self)
+        self.print2(self)
+        self.print3()
+        self.print4(self)
+
+
+class InterfaceTwo(object):
+
+    interfaceTwo = "InterfaceTwo - > Interface allow to have constant variables"
+
+    def InterfaceTwo():
+        return "InterfaceTwo - > We can use interface method as private"
+
+    def print1():
+        pass
+
+    @classmethod
+    def print2():
+        print("InterfaceTwo - > This is the different between default and DEFAULT")
+
+    @staticmethod
+    def print3():
+        print("InterfaceTwo - > This is the different between default and STATIC")
+
+    @staticmethod
+    def print4():
+        print("InterfaceTwo - > This is the different between static and STATIC")
+
+    @staticmethod
+    def print5():
+        print("InterfaceTwo - > This is the different between abstract and STATIC")
+
+    @classmethod
+    def print7(self):
+        print(self.InterfaceTwo())
+        self.print1(self)
+        self.print2(self)
+        self.print3()
+        self.print4(self)
+        self.print5(self)
+
+
+class ImplementationOne(object):
+
+    @abstractmethod
+    def ImplementationOne(self):
+        pass
+
+
+class ImplementationTwo(ImplementationOne, InterfaceOne, InterfaceTwo):
+
+    interfaceTwo = "We can also override constant variables"
+
+    def ImplementationOne(self):
+        print(InterfaceOne.interfaceOne)
+        print(InterfaceTwo.interfaceTwo)
+
+    def print1(self):
+        print("Implementation Two - > If we have default and abstract method, we have to override it, becaue we have to implement abstract")
+
+    def print2(self):
+        print("Implementation Two - > If we have two default method, we have to override it, becaue we have to decide which one is the most important")
+
+    def print4(self):
+        print("Implementation Two - > We have two static method")
+
+    def print5(self):
+        print("Implementation Two - > We have static and abstract method")
+
+    def print8(self):
+        InterfaceOne.print4(self)
+        InterfaceTwo.print3()
+        InterfaceTwo.print4()
+        InterfaceTwo.print5()
+
+    class ImplementationThree:
+
+        def ImplementantionThree(self):
+            print("We can have class in class, that call inner class")
     
-    def start(self):
-        self.__isOn = True
-
-    def stop(self):
-        self.__isOn = False
--- 012. Interfaces, Abstract and Inner Classes/Main.py
+++ 012. Interfaces, Abstract and Inner Classes/Main.py
@@ -1,20 +1,57 @@
 #Interfaces, Abstract and Inner Class
 
-from Class import Engine
-from Class import Car
-from Class import Gearbox
+from Class import *
 
+nameToTest1 = ImplementationTwo()
 
-kyr435 = Engine(horsePower=450, cylinders=4, valve=16)
-honda = Car(4, 1050, 4, Gearbox(transmission=20, maxGear=5), kyr435)
+nameToTest2 = ImplementationTwo().ImplementationThree()
+nameToTest2.ImplementantionThree()
+print()
 
-honda.carStart()
-honda.carParameter()
-honda.changeSpeed(15)
-honda.carParameter()
-honda.changeSpeed(75)
-honda.carParameter()
+nameToTest1.ImplementationOne()
+print("Print1: ")
+nameToTest1.print1()
+print("Print2: ")
+nameToTest1.print2()
+print("Print3: ")
+nameToTest1.print3()
+print("Print4: ")
+nameToTest1.print4()
+print("Print5: ")
+nameToTest1.print5()
 
+print("\nPrint InterfaceOne: \n")
+nameToTest1.print6()
+
+print("\nPrint InterfaceTwo: \n")
+nameToTest1.print7()
+
+print("\nWe have also access to static method from interfaces in class:")
+nameToTest1.print8()
+
+print("\nWe have also access to variables in interfaces")
+print(nameToTest1.interfaceOne)
+print(super(ImplementationTwo, nameToTest1).interfaceTwo)
+
+print("\nWe have also access to default method in interfaces")
+print("InterfaceOne:")
+print("Print1: ")
+super(ImplementationTwo, nameToTest1).print1()
+print("Print2: ")
+super(ImplementationTwo, nameToTest1).print2()
+print("Print3: ")
+super(ImplementationTwo, nameToTest1).print3()
+print("Print4: ")
+super(ImplementationTwo, nameToTest1).print4(nameToTest1)
+print("Print6: ")
+super(ImplementationTwo, nameToTest1).print6()
+print("\nInterfaceTwo:")
+print("Print1: ")
+InterfaceTwo.print1()
+print("Print2: ")
+super(ImplementationTwo, nameToTest1).print2()
+print("Print7: ")
+super(ImplementationTwo, nameToTest1).print7()
 
 # End of file
 print()
